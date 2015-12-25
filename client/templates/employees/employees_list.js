604b3ef2efd3cf57c4f934052e464020750142b7
Jonathan Katz <jonathan.a.katz@gmail.com>
2015-12-24 16:42:18 -0800

    wip

--- client/templates/employees/employees_list.js
+++ client/templates/employees/employees_list.js
@@ -3,13 +3,17 @@ var employeesData = [
     "name": 'Jonathan Katz',
     "email": 'jonathan@vulcun.com',
     "squad": 'Vulcun',
-    "join_date": "April 2015"
+    "join_date": "April 2015",
+    "product": "Vulcun",
+    "reviews": '5'
   },
   {
     "name": 'Ben Lee',
     "email": 'ben@vulcun.com',
     "squad": 'Vulcun',
-    "join_date": "June 2015"
+    "join_date": "June 2015",
+    "product": "Vulcun",
+    "reviews": '5'
   }
 ];
 
